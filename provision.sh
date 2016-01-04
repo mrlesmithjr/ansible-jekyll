30485c08856053614469e9d3cef6ea4726d21590
Larry Smith Jr <mrlesmithjr@gmail.com>
2016-01-04 18:33:15 -0500

    Cleaned up script
    Signed-off-by: Larry Smith Jr <mrlesmithjr@gmail.com>

--- provision.sh
+++ provision.sh
@@ -3,8 +3,5 @@ sudo apt-get update
 sudo apt-get install -y git python-pip python-dev
 sudo pip install jinja2
 sudo pip install ansible
-#sudo mkdir -p /opt/ansible-playbooks/roles
-#sudo git clone https://github.com/mrlesmithjr/ansible-jekyll.git /opt/ansible-playbooks/roles/ansible-jekyll
-#sudo cp /vagrant/playbook.yml /opt/ansible-playbooks
 sudo ansible-galaxy install -r /vagrant/requirements.yml -f
 ansible-playbook -i "localhost," -c local /vagrant/playbook.yml
