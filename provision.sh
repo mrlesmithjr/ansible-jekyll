e6d3e3a2f52a6fac176b757598c376b1a62178ad
Larry Smith Jr <mrlesmithjr@gmail.com>
2016-01-04 17:05:51 -0500

    Added provision script and playbook
    Signed-off-by: Larry Smith Jr <mrlesmithjr@gmail.com>

--- provision.sh
+++ provision.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-apt-get update
-apt-get install -y python-pip python-dev
-pip install jinja2
-pip install ansible
-
+sudo apt-get update
+sudo apt-get install -y git python-pip python-dev
+sudo pip install jinja2
+sudo pip install ansible
+sudo mkdir -p /opt/ansible-playbooks/roles
+sudo git clone https://github.com/mrlesmithjr/ansible-jekyll.git /opt/ansible-playbooks/roles/ansible-jekyll
+sudo cp /vagrant/playbook.yml /opt/ansible-playbooks
+ansible-playbook -i "localhost," -c local /opt/ansible-playbooks/playbook.yml
